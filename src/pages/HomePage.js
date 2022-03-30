c1a530000e9d5dce1370c7237887cbc9a2cb9b03
Ryifki Yidan <ryifkiyidan@gmail.com>
2022-03-30 22:07:08 +0700

    Finish

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,11 @@ function HomePage() {
       <h1 className="mb-3">Home</h1>
       <code className="text-2">Aplikasi ini merupakan jawaban dari nomor 3, 4, dan 5</code>
       <br />
-      <code className="text-2"> Tiap nomor memiliki halaman masing-masing yang dapat diakses melalui navbar diatas</code>
+      <code className="text-2">Tiap nomor memiliki halaman masing-masing yang dapat diakses melalui navbar diatas</code>
+      <br />
+      <code className="text-2">MOCK_DATA.json untuk data pada tabel halaman nomor 3 ada di folder "src"</code>
+      <br />
+      <code>by Ryifki Yidan</code>
     </Container>
   );
 }
